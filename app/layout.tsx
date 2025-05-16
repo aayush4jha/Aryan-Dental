34f0e78cddf2717aaf557ade17b8537e61a315a4
Aayush Jha <aayush4jha@gmail.com>
2025-05-16 15:33:38 +0530

    Location fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     "Braces",
     "Teeth Whitening",
     "Oral Care",
-    "Dentist in Surat"
+    "Dentist in Darbhanga"
   ],
   authors: [{ name: "Aryan Dental Team" }],
   viewport: "width=device-width, initial-scale=1.0",
